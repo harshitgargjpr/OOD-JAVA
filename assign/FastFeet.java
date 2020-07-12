130fd57b6530d312d90422ea46a90966340f4875
Harshit Garg <harshitgargjpr@gmail.com>
2020-07-12 17:17:47 +0530

    As we have Mountain bike also in demand, its best to split bicycle class and extend other bikes from it. bicycle will contain the featues which is common to all bikes. Also making it abstract because we have specific categories of bikes user to pick from.

--- assign/FastFeet.java
+++ assign/FastFeet.java
@@ -1,28 +1,74 @@
 package assign;
 
-class Bicycle{
-    private String size, tapeColor;
+abstract class Bicycle{
+    private String size, chain;
+    protected double tireSize;
 
-    public Bicycle(String size, String tapeColor){
+    public Bicycle(String size, String chain, double tireSize){
         this.size = size;
+        this.chain = chain == null ? default_chain() : chain;
+        this.tireSize = Double.isNaN(tireSize) ? default_tire_size() : tireSize;
+    }
+
+    public void showSpares(){
+        System.out.println("chain: " + chain);
+        System.out.println("tireSize: " + tireSize);
+    }
+
+    protected String default_chain(){
+        return "10-speed";
+    }
+
+    abstract protected double default_tire_size();
+}
+
+class RoadBike extends Bicycle{
+    private String tapeColor;
+
+    public RoadBike(String tapeColor, String chain, String size, double tireSize){
+        super(size, chain, tireSize);
         this.tapeColor = tapeColor;
     }
 
     public void showSpares(){
-        System.out.println("chain: 10-speed");
-        System.out.println("tire size: 23");
+        super.showSpares();
         System.out.println("tapeColor: " + tapeColor);
     }
 
-    public String getSize(){
-        return size;
+    protected double default_tire_size(){
+        return 23;
+    }
+}
+
+class MountainBike extends Bicycle{
+    private String front_shock, rear_shock;
+
+    public MountainBike(String front_shock, String rear_shock, String chain, String size, double tireSize){
+        super(size, chain, tireSize);
+        this.front_shock = front_shock;
+        this.rear_shock = rear_shock;
+    }
+
+    public void showSpares(){
+        super.showSpares();
+        System.out.println("rear_shock: " + rear_shock);
+    }
+
+    protected double default_tire_size(){
+        return 2.1;
+    }
+
+    protected String default_chain(){
+        return "15-speed";
     }
 }
 
 public class FastFeet {
     public static void main(String[] args){
-        Bicycle myBike = new Bicycle("M", "Red");
-        System.out.println(myBike.getSize());
-        myBike.showSpares();
+        MountainBike m1 = new MountainBike("f1", "r1", null, "L", Double.NaN);
+        m1.showSpares();
+
+        RoadBike r1 = new RoadBike("purple", "20-speed", "M", 53.26);
+        r1.showSpares();
     }
 }
