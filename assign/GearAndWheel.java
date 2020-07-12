d9ec151e2657d1eee2a84254cef810da66c3e86c
Harshit Garg <harshitgargjpr@gmail.com>
2020-07-12 13:42:54 +0530

    Adding new feature to compare bikes with different in sizes of wheel, Also maintained uniformity so that previous code do not break with some assumptions on default size of rim and tire.

--- assign/GearAndWheel.java
+++ assign/GearAndWheel.java
@@ -1,15 +1,38 @@
 package assign;
 
 class Gear{
+    private class Wheel{
+        private double rim, tire;
+        private Wheel(double rim, double tire){
+            this.rim = rim;
+            this.tire = tire;
+        }
+
+        private double diameter(){
+            return rim + 2*tire;
+        }
+    }
+
     private int chainring, cog;
+    private Wheel wheel;
 
     public Gear(int chainring, int cog){
+        // assuming 50 and 2 are default values for rim and tire
+        this(chainring, cog, 50, 2);    
+    }
+
+    public Gear(int chainring, int cog, double rim, double tire){
         this.chainring = chainring;
         this.cog = cog;
+        wheel = new Wheel(rim, tire);
     }
 
     public double ratio(){
         return chainring/(double)cog;
+    }
+
+    public double gearInches(){
+        return ratio() * wheel.diameter();
     }
 }
 
@@ -20,5 +43,8 @@ public class GearAndWheel{
 
         System.out.println(gear1.ratio());
         System.out.println(gear2.ratio());
+
+        Gear gear3 = new Gear(52, 11 ,26, 1.5);
+        System.out.println(gear3.gearInches());
     }
 }
